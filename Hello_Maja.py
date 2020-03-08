0af5a8b851b62ab0417a759e9a4d1f289f9ab354
zivekovic <zvonimir.ivekovic@hotmail.com>
2020-03-08 17:09:36 +0100

    Shit commit

--- Hello_Maja.py
+++ Hello_Maja.py
@@ -1,3 +1,8 @@
-print ("Hello Maja")
+print("Hello Maja")
 for i in range(83):
     print("Di su mafini")
+
+a1 = "blabla"
+a2 = bytes("blabla)
+print(f"len(a1) = {len(a1)}")
+print(f"len(a2) = {len(a2)}")
